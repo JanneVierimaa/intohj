e8e967b7e5c1f723ef904343f20f752aeafc8808
Janne Vierimaa <t8vija01@students.oamk.fi>
2019-09-03 20:08:52 +0300

    html runko menulle valmis

---
+++ menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+   <div id="header">
+     This is the header
+    </div>
+   <div id="menu">
+     This is the menu
+     </div>
+   <div id="content">
+     This is the content
+     </div>
+   <div id="footer">
+     This is the footer
+     </div>
+
+  </body>
+</html>
